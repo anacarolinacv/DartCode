40da5468aaa85bb3fa0e7445f8d6b50df471f05b
anacarolinacv <ana.vasconcelos@ccc.ufcg.edu.br>
2020-02-20 11:07:33 -0300

    começando

---
+++ classe.dart
@@ -0,0 +1,40 @@
+// aprendendo dart (inicio)
+
+class Animal {
+  int patas;
+  int idade;
+  int validacao;
+  final String _tipo;
+
+  Animal(this patas, int idade, String tipo) {
+    this.raca = raca;
+    this.idade = idade;
+    this.tipo = _tipo;
+    this.validacao = 0;
+  
+  }
+
+  void adicionaIdade(int ano) {
+    idade += ano;
+  } 
+
+  void acrescentaValidacao(int valida) {
+    validacao += valida;
+  }
+  void decrescentaValidacao(int valida) {
+    validacao -= valida;
+  }
+
+  String get tipo => _tipo;
+  
+  String toString() => 'Animal com $patas patas e $idade de idade.';
+
+}
+
+
+void main() {
+
+  var animal1 = Animal(4, 2);
+  print(animal1);
+
+}
